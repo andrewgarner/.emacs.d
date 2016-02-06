c02b2957b88081f72d559ff97094daa2d1b03f4d
Andrew Garner <andrew@andrewgarner.com>
2016-02-06 10:37:45 +0000

    Configure UI

--- core/surefire-ui.el
+++ core/surefire-ui.el
@@ -12,5 +12,44 @@
 
 ;;; Code:
 
+;; the toolbar is just a waste of valuable screen estate
+;; in a tty tool-bar-mode does not properly auto-load, and is
+;; already disabled anyway
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+
+;; disable menu bar
+(menu-bar-mode -1)
+
+;; disable Scrollbar
+(if window-system (set-scroll-bar-mode 'nil))
+
+;; disable startup screen
+(setq inhibit-startup-screen t)
+
+;; nice scrolling
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+
+;; mode line settings
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+
+;; enable y/n answers
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; more useful frame title, that show either a file or a
+;; buffer name (if the buffer isn't visiting a file)
+(setq frame-title-format
+      '("Emacs - " (:eval (if (buffer-file-name)
+                              (abbreviate-file-name (buffer-file-name))
+                            "%b"))))
+
+;; show the cursor when moving after big movements in the window
+(surefire-require-package 'beacon)
+(beacon-mode +1)
+
 (provide 'surefire-ui)
 ;;; surefire-ui.el ends here
