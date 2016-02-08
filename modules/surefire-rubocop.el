ba4e2fcb0ff94e8044bb9a0870ea3a661628f326
Andrew Garner <andrew@andrewgarner.com>
2016-02-08 12:59:38 +0000

    Add Rubocop module

---
+++ modules/surefire-rubocop.el
@@ -0,0 +1,19 @@
+;;; surefire-rubocop.el --- Emacs: Rubocop module.
+;;
+;; Copyright © 2016 SureFireThing Ltd
+;;
+;; Author: Andrew Garner <andrew@surefirething.co.uk>
+;; URL: https://github.com/surefire/.emacs.d
+
+;;; Commentary:
+
+;; Some basic configuration for Rubocop.
+
+;;; Code:
+
+(surefire-require-package 'rubocop)
+
+(add-hook 'ruby-mode-hook 'rubocop-mode)
+
+(provide 'surefire-rubocop)
+;;; surefire-rubocop.el ends here
